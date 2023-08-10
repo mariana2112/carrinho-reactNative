c1b7a90df3b9174729664df5aafc80341c933b57
mariana2112 <marianaroveferreira@gmail.com>
2023-08-10 08:53:58 -0300

    image

--- App.tsx
+++ App.tsx
@@ -1,21 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, StatusBar, SafeAreaView } from 'react-native';
+import Carrinho from './src/pages/Carrinho';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Carrinho</Text>
-      <StatusBar style="auto" />
-    </View>
+  <SafeAreaView>
+     <StatusBar/>
+      <Carrinho/>
+  </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+
 });
  
--- src/pages/Carrinho/index.tsx
+++ src/pages/Carrinho/index.tsx
@@ -1,6 +1,23 @@
 import React from "react";
-import { Text } from "react-native";
+import EvilIcons from "@expo/vector-icons/EvilIcons";
+import Feather from "@expo/vector-icons/Feather";
+import { Text, View, StyleSheet, Image } from "react-native";
+import topo from '../../../assets/topo.png';
 
-export default function Carrinho(){
-    return( <Text>Carrinho de Compras</Text>);
+export default function Carrinho() {
+  return(
+      <View style={styles.container}>
+         <Image source={topo} style={styles.topo}/>
+      </View>
+       );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#eeee',
+      },
+    topo:{
+        width: '100%',  
+    }
+  });
