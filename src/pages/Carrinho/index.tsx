8ae62bbcfe23419b464d7e05892a6214ae264869
mariana2112 <marianaroveferreira@gmail.com>
2023-08-17 08:57:41 -0300

    design process

--- src/pages/Carrinho/index.tsx
+++ src/pages/Carrinho/index.tsx
@@ -1,23 +1,38 @@
 import React from "react";
-import EvilIcons from "@expo/vector-icons/EvilIcons";
-import Feather from "@expo/vector-icons/Feather";
-import { Text, View, StyleSheet, Image } from "react-native";
+import { Text, View, StyleSheet, Image, Dimensions } from "react-native";
 import topo from '../../../assets/topo.png';
+
+const width = Dimensions.get('screen').width;
 
 export default function Carrinho() {
   return(
-      <View style={styles.container}>
+    <>
          <Image source={topo} style={styles.topo}/>
-      </View>
+         <Text style={styles.titulo}>Detalhes do Carrinho</Text>
+         <View>
+           <Text>Carrinho de Compras</Text>
+           <Text>Fazenda Etec Cidade do Livro</Text>
+           <Text>carrinho de Compras</Text>
+           <Text>Uma cesta com produtos selecionados cuidadosamente da fazenda direto para a sua cozinha.</Text>
+           <Text>R$ 40,00</Text>
+         </View>
+      </>
        );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#eeee',
-      },
     topo:{
         width: '100%',  
+        height: 578 / 786 * width,
+    },
+    titulo:{
+      width: '100%',
+      position: 'absolute',
+      color: 'white',
+      textAlign: 'center',
+      fontSize: 24,
+      lineHeight: 26,
+      fontWeight: 'bold',
+      padding: 16
     }
   });
