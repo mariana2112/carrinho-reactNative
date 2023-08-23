2799cde961ea329b7be8dca14dd95eec64e8113d
mariana2112 <marianaroveferreira@gmail.com>
2023-08-23 15:47:30 -0300

    logo

--- src/pages/Carrinho/index.tsx
+++ src/pages/Carrinho/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Text, View, StyleSheet, Image, Dimensions } from "react-native";
 import topo from '../../../assets/topo.png';
+import logo from '../../../assets/logo.png';
 
 const width = Dimensions.get('screen').width;
 
@@ -9,9 +10,14 @@ export default function Carrinho() {
     <>
          <Image source={topo} style={styles.topo}/>
          <Text style={styles.titulo}>Detalhes do Carrinho</Text>
-         <View>
-           <Text>Carrinho de Compras</Text>
+         <View style={styles.carrinho}>
+           <Text  style={styles.nome}>Carrinho de Compras</Text>
+
+           <View style={styles.fazenda}>
+             <Image source={logo}/>
            <Text>Fazenda Etec Cidade do Livro</Text>
+           </View>
+
            <Text>carrinho de Compras</Text>
            <Text>Uma cesta com produtos selecionados cuidadosamente da fazenda direto para a sua cozinha.</Text>
            <Text>R$ 40,00</Text>
@@ -34,5 +40,19 @@ const styles = StyleSheet.create({
       lineHeight: 26,
       padding: 16,
       fontFamily: 'MontserratItalic'
+    },
+    carrinho:{
+      paddingVertical: 8,
+      paddingHorizontal: 16
+    },
+    nome:{
+      fontSize:26,
+      lineHeight:42,
+      color:"#464646",
+      fontFamily: 'MontserratBold'
+    },
+    fazenda:{
+      flexDirection: "row",
+      paddingVertical: 12
     }
   });
